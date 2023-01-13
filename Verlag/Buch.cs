5246a81da355c5c6b38082e9676d64d925376732
nikolasgerz <moondoon05@gmail.com>
2023-01-13 11:45:08 +0100

    Buchklasse erstellt

---
+++ Verlag/Buch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verlag
+{
+    public class Buch
+    {
+        private string titel;
+        private string autor;
+
+        private int auflage;
+
+        public Buch(string titel, string autor, int auflage)
+        {
+            this.titel = titel;
+            this.autor = autor;
+            this.auflage = auflage;
+        }
+
+        public string Titel
+        {
+            get { return titel; } 
+        }
+
+        public string Autor
+        {
+            get { return autor;  }
+        }
+
+        public int Auflage
+        {
+            get { return auflage; }
+        }
+
+
+    }
+}
